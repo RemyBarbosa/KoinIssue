8df354ed98595752fa424e257fe2edc336e870d5
Stephane MORA <stephane.mora@gmail.com>
2018-04-20 14:54:36 +0200

    WIP new demo screen

--- alarm_v5/src/main/kotlin/fr/radiofrance/alarm/RfAlarmManager.kt
+++ alarm_v5/src/main/kotlin/fr/radiofrance/alarm/RfAlarmManager.kt
@@ -5,4 +5,6 @@ class RfAlarmManager {
     val message = "Hello from library"
 
 
+
+
 }
--- alarmdemo_v5/src/main/kotlin/fr/radiofrance/alarm/MainActivity.kt
+++ alarmdemo_v5/src/main/kotlin/fr/radiofrance/alarm/MainActivity.kt
@@ -1,7 +1,10 @@
 package fr.radiofrance.alarm
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import fr.radiofrance.alarm.utils.BatteryOptimizationUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_textview.text = RfAlarmManager().message
+        main_version_textview.text = "OS version :  ${Build.VERSION.RELEASE} (${Build.VERSION.SDK_INT})"
+        main_ignore_battery_optim_textview.text = "Ignore battery optimization :  ${BatteryOptimizationUtils.isIgnoringBatteryOptimizations(applicationContext)}"
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            main_ignore_battery_optim_button.visibility = View.VISIBLE
+            main_ignore_battery_optim_button.setOnClickListener {
+                BatteryOptimizationUtils.showBatteryOptimizationDialog(this, getString(R.string.app_name))
+            }
+        }
+
+
+        //RfAlarmManager().message
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        main_ignore_battery_optim_textview.text = "Ignore battery optimization :  ${BatteryOptimizationUtils.isIgnoringBatteryOptimizations(applicationContext)}"
     }
 }
