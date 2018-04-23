931e5572be968938c9c45710108fb3d9235ffa9e
Stephane MORA <stephane.mora@gmail.com>
2018-04-23 16:05:03 +0200

    WIP Alarm activty show on locked device

--- alarm_v5/src/main/kotlin/fr/radiofrance/alarm/activity/AlarmActivity.kt
+++ alarm_v5/src/main/kotlin/fr/radiofrance/alarm/activity/AlarmActivity.kt
@@ -2,8 +2,11 @@ package fr.radiofrance.alarm.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
 import fr.radiofrance.alarm.R
 import kotlinx.android.synthetic.main.activity_alarm.*
 import java.text.SimpleDateFormat
@@ -26,6 +29,24 @@ class AlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Showing on lock screen
+        setShowWhenLocked(true)
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Hide Navigation bar
+            val decorView = getWindow().decorView
+            decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE)
+        }
+
         setContentView(R.layout.activity_alarm)
 
         main_hour_textview.text = SimpleDateFormat("hh:mm:ss", Locale.getDefault()).format(Date())
