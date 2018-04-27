d0c39bd73483863825be67c074a78e700c035079
Stephane MORA <stephane.mora@gmail.com>
2018-04-27 14:44:15 +0200

    WIP add radio on succeed callback

--- alarmdemo_v5/src/main/kotlin/fr/radiofrance/alarm/callback/AppAlarmCallbackReceiver.kt
+++ alarmdemo_v5/src/main/kotlin/fr/radiofrance/alarm/callback/AppAlarmCallbackReceiver.kt
@@ -1,15 +1,39 @@
 package fr.radiofrance.alarm.callback
 
 import android.content.Context
+import android.os.Handler
+import android.os.Message
 import fr.radiofrance.alarm.broadcast.AlarmCallbackReceiver
 
 class AppAlarmCallbackReceiver : AlarmCallbackReceiver() {
 
     override fun onRang(context: Context, callback: OnRangCustomCallback) {
 
-        DemoPlayer.getInstance(context).play("http://direct.fipradio.fr/live/fip-lofi.mp3")
+        val player = DemoPlayer.getInstance(context)
 
-        callback.onRangCustomSucceed()
+        player.play("http://direct.fipradio.fr/live/fip-lofi.mp3")
+
+        CheckPlayerHandler(player, callback).check()
+    }
+
+    private class CheckPlayerHandler(private val player: DemoPlayer, private val callback: OnRangCustomCallback) : Handler() {
+
+        internal fun check() {
+            sendMessage(obtainMessage(MESSAGE_WHAT))
+        }
+
+        override fun handleMessage(msg: Message) {
+            if (player.isPlaying) {
+                callback.onRangCustomSucceed()
+                return
+            }
+            sendMessageDelayed(obtainMessage(MESSAGE_WHAT), CHECK_WAKE_UP_RETRY_DELAY_MS)
+        }
+
+        companion object {
+            private const val CHECK_WAKE_UP_RETRY_DELAY_MS = 500L
+            private const val MESSAGE_WHAT = 0
+        }
     }
 
 }
